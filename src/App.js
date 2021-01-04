31c0aa218881c5e5f0c154d88a58b061fae14085
Dominik Ostrowski <ostrowski.dominik@gmail.com>
2021-01-04 15:15:54 +0100

    main layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './css/style.css';
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <div className="app">
+      <header className="header"></header>
+      <div className="technologies"></div>
+      <div className="projects"></div>
+      <div className="motto"></div>
+      <div className="interests"></div>
+      <div className="contact"></div>
+      <footer className="footer"></footer>
+    </div >
   );
 }
 
