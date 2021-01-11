085a5462b1d6accbb99741af209cf9e7b626ea44
Dominik Ostrowski <ostrowski.dominik@gmail.com>
2021-01-11 09:17:40 +0100

    Technologies component -  article changes

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 
-import Article from './Article/Article.jsx';
+import Article from './Article.jsx';
 
-import photo1 from '../assets/images/services1.png';
-import photo2 from '../assets/images/services2.png';
-import photo3 from '../assets/images/services3.png';
-import photo4 from '../assets/images/services4.png';
-import photo5 from '../assets/images/services5.png';
-import photo6 from '../assets/images/services6.png';
+import photo1 from '../../assets/images/services1.png';
+import photo2 from '../../assets/images/services2.png';
+import photo3 from '../../assets/images/services3.png';
+import photo4 from '../../assets/images/services4.png';
+import photo5 from '../../assets/images/services5.png';
+import photo6 from '../../assets/images/services7.png';
+import photo7 from '../../assets/images/services8.png';
+import photo8 from '../../assets/images/services9.png';
 
 const articleDetails = [
   {
@@ -26,18 +28,18 @@ const articleDetails = [
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis sunt ratione debitis voluptatum ut ea tempore dignissimos labore deleniti ad voluptate maiores, quas provident iusto quibusdam repellat non doloribus.'
   },
   {
-    img: photo5,
-    title: 'React.JS',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis sunt ratione debitis voluptatum ut ea tempore dignissimos labore deleniti ad voluptate maiores, quas provident iusto quibusdam repellat non doloribus.'
-  },
-  {
-    img: photo4,
-    title: 'React Native',
+    img: [photo5, photo4],
+    title: 'React.JS / React Native',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis sunt ratione debitis voluptatum ut ea tempore dignissimos labore deleniti ad voluptate maiores, quas provident iusto quibusdam repellat non doloribus.'
   },
   {
     img: photo6,
     title: 'Node.JS',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis sunt ratione debitis voluptatum ut ea tempore dignissimos labore deleniti ad voluptate maiores, quas provident iusto quibusdam repellat non doloribus.'
+  },
+  {
+    img: [photo7, photo8],
+    title: 'MongoDB / Google Firebase',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis sunt ratione debitis voluptatum ut ea tempore dignissimos labore deleniti ad voluptate maiores, quas provident iusto quibusdam repellat non doloribus.'
   },
 ]
